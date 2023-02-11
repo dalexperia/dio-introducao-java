0383129e44854c872d580f369e6125ec5e402dfd
Daniel Alex <dalexperia@gmail.com>
2023-02-11 13:10:49 -0300

    Novas aulas sobre tipos de variáveis

--- src/edu/daniel/tiposvariaveis/TiposVariaveis.java
+++ src/edu/daniel/tiposvariaveis/TiposVariaveis.java
@@ -2,6 +2,22 @@ package edu.daniel.tiposvariaveis;
 
 public class TiposVariaveis {
     public static void main(String[] args) {
+        /*
+        Tipos de dados primitivos, não são considerados como objetos. Ficam na memory stack
+        ‘byte’, 'short', int, long, float, double (mais comum)
+        <tipo> <nome-variavel> <atribuicao-opcional>
+         */
+        int idade;
+        int anoFabricacao = 2022;
+        double salarioMinimo = 2500.33;
+        float pi = 3.14F;
+        long cpf = 2249641439L;
+        short numeroCurto = 1;
+
+        // CONSTANTES coloca-se final e escreva o nome da variável com caixa alta
+
+        final float PI = 3.14F;
+        final int DESCOBRIMENTO_BRASIL = 1500;
 
     }
 }
